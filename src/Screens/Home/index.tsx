d2e3f6ee0bacd48fd58fbfe5ee93dd909a09ea85
Liriali4 <lirialia51@gmail.com>
2025-03-06 20:20:45 +0100

    Criada a base da Home

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -10,14 +10,101 @@ export default function HomePage(): JSX.Element {
             <Header />
             <Flex
                 p={'10px 30px'}
-                h={'500px'}
+                h={'450px'}
                 bg={'#d4b6ef'}
                 align={'center'}
             >
                 <Text fontSize={50} fontWeight={'bold'}>banner</Text>
             </Flex>
-            <Box>
-                <Text fontSize={50} fontWeight={'bold'}>Body</Text>
+            <Box p={'30px'}>
+                <Flex py={10} justify={'space-around'}>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        bg={'#d4b6ef'}
+                        borderRadius={10}
+                    >
+                    </Box>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        borderRadius={10}
+                        alignContent={'center'}
+                    >
+                        <Text fontSize={30} fontWeight={'bold'}>Body</Text>
+                        <Text fontSize={20}>
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                        </Text>
+                    </Box>
+                </Flex>
+                <Flex py={10} justify={'space-around'}>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        borderRadius={10}
+                        alignContent={'center'}
+                    >
+                        <Text fontSize={30} fontWeight={'bold'}>Body</Text>
+                        <Text fontSize={20}>
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                        </Text>
+                    </Box>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        bg={'#d4b6ef'}
+                        borderRadius={10}
+                    >
+                    </Box>
+                </Flex>
+                <Flex py={10} justify={'space-around'}>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        bg={'#d4b6ef'}
+                        borderRadius={10}
+                    >
+                    </Box>
+                    <Box
+                        w={'500px'}
+                        h={'300px'}
+                        borderRadius={10}
+                        alignContent={'center'}
+                    >
+                        <Text fontSize={30} fontWeight={'bold'}>Body</Text>
+                        <Text fontSize={20}>
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                            Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                        </Text>
+                    </Box>
+                </Flex>
+            </Box>
+            <Box
+                p={'0px 30px'}
+                h={'300px'}
+                borderRadius={10}
+                alignContent={'center'}
+                textAlign={'center'}
+            >
+                <Text fontSize={30} fontWeight={'bold'}>Body</Text>
+                <Text px={'15%'} fontSize={20}>
+                    Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                    Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                    Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                    Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                    Texto aleatório Texto aleatório Texto aleatório Texto aleatório
+                </Text>
             </Box>
             <Footer />
         </Box>
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,11 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer(): JSX.Element {
     return (
-        <Box>
-            <Text fontSize={50} fontWeight={'bold'}>Footer</Text>
+        <Box 
+            h={'80px'}
+            bg={'#d4b6ef'}
+        >
+            <Text fontSize={20} fontWeight={'bold'}>Footer</Text>
         </Box>
     )
 }
